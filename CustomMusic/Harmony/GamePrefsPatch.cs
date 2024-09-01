2fbc5d78bd79373076741da1fcf03ad94db8a7e0
Rizart Dokollari <r.dokollari@gmail.com>
2024-09-01 22:00:41 +0100

    fix: Remove debug logs.

--- CustomMusic/Harmony/GamePrefsPatch.cs
+++ CustomMusic/Harmony/GamePrefsPatch.cs
@@ -8,12 +8,14 @@ namespace CustomMusic.Harmony
     [HarmonyPatch(typeof(GamePrefs))]
     public static class GamePrefsPatch
     {
+        private static readonly ILogger Logger = new Logger();
+        
         [HarmonyPatch(nameof(GamePrefs.Save), new Type[] { })]
         [HarmonyPatch(nameof(GamePrefs.Save), new Type[] { typeof(string) })]
         [HarmonyPatch(nameof(GamePrefs.Save), new Type[] { typeof(string), typeof(List<EnumGamePrefs>) })]
         public static void Postfix()
         {
-            Debug.Log("GamePrefsPatch: Postfix executed after Save.");
+            Logger.Info("GamePrefsPatch: Postfix executed after Save.");
 
             var isEnabled = GamePrefs.GetBool(EnumGamePrefs.OptionsDynamicMusicEnabled);
 
