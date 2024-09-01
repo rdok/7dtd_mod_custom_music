e2ea66168c06f480c9172e086b84d53cc04701ec
Rizart Dokollari <r.dokollari@gmail.com>
2024-09-01 20:19:59 +0100

    feat: Control music volume & toggling on/off upon game preferences changed event.

---
+++ CustomMusic/Harmony/GamePrefsPatch.cs
@@ -0,0 +1,25 @@
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomMusic.Harmony
+{
+    [HarmonyPatch(typeof(GamePrefs))]
+    public static class GamePrefsPatch
+    {
+        [HarmonyPatch(nameof(GamePrefs.Save), new Type[] { })]
+        [HarmonyPatch(nameof(GamePrefs.Save), new Type[] { typeof(string) })]
+        [HarmonyPatch(nameof(GamePrefs.Save), new Type[] { typeof(string), typeof(List<EnumGamePrefs>) })]
+        public static void Postfix()
+        {
+            Debug.Log("GamePrefsPatch: Postfix executed after Save.");
+
+            var isEnabled = GamePrefs.GetBool(EnumGamePrefs.OptionsDynamicMusicEnabled);
+
+            CustomMusicPlayer.IsCustomMusicEnabled = isEnabled;
+            CustomMusicPlayer.UpdateVolume();
+            ;
+        }
+    }
+}
