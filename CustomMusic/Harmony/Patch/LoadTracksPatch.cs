348c5f83acd24d7ff9d93fd49ceff8844cf04cf5
Rizart Dokollari <r.dokollari@gmail.com>
2024-09-07 23:41:54 +0100

    fix: Load netstandard from nuget.

--- CustomMusic/Harmony/Patch/LoadTracksPatch.cs
+++ CustomMusic/Harmony/Patch/LoadTracksPatch.cs
@@ -25,15 +25,12 @@ namespace CustomMusic.Harmony.Patch
             var modPath = Assembly.GetExecutingAssembly().Location;
             var modDirectory = Path.GetDirectoryName(modPath);
             var missingModPathError = new InvalidOperationException($"Missing tracks directory: {modPath}");
-            
-#if DEBUG
             var musicDirectory = Path.Combine(modDirectory ?? throw missingModPathError, "DebugAmbientTracks");
-#endif
-            
+             
 #if RELEASE
             var musicDirectory = Path.Combine(modDirectory ?? throw missingModPathError, "AmbientTracks");
 #endif
-            Logger.Debug($"LoadTracks: Checking for music directory at {musicDirectory}.");
+            Logger.Debug($"LoadTracks: Checking for music directory at {musicDirectory}. ");
 
             string[] validExtensions = { ".mp3", ".wav", ".aiff", ".flac" };
 
